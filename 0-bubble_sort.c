17125fc8614679fae66a63aa07416b046a577475
Elijah <eachala5@gmail.com>
2024-01-19 08:43:44 -0800

    00

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 /**
  * bubble_sort - implements bubble sort algorithm
- * @arrary: 1p
+ * @array: 1p
  * @size: 2p
  */
 void bubble_sort(int *array, size_t size)
@@ -9,9 +9,9 @@ void bubble_sort(int *array, size_t size)
 size_t i;
 size_t j;
 size_t temp;
-for(i = 0; i < size; ++i)
+for (i = 0; i < size; ++i)
 {
-for(j = 0; j < (size - i -1); ++j)
+for (j = 0; j < (size - i - 1); ++j)
 {
 if (array[j] > array[j + 1])
 {
