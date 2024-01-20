ffb2eee2b1d3071b4df2f6279d33cf5342692f1c
Elijah <eachala5@gmail.com>
2024-01-20 01:26:45 -0800

    2

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * selection_sort -iplements selection sort
+ * @array: 1p
+ * @size: 2p
+ */
+void selection_sort(int *array, size_t size)
+{
+int i, j, min, idx;
+i = 0;
+while ((size_t)i < size)
+{
+j = i;
+min = array[j];
+idx = j;
+while ((size_t) ++j < size)
+{
+if (min > array[j])
+{
+min = array[j];
+idx = j;
+}
+++j;
+}
+array[idx] = array[i];
+array[i] = min;
+print_array(array, size);
+++i;
+}
+}
